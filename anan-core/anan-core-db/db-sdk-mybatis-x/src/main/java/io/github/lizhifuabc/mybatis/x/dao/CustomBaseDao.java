62eb855afc28336ba26bda914d79165c3804d6d2
lizhifu <lizhifuabc@163.com>
2025-04-16 17:24:49 +0800

    feat(db-sdk-mybatis-x): 新增自定义基础 DAO 和 Mapper接口

---
+++ anan-core/anan-core-db/db-sdk-mybatis-x/src/main/java/io/github/lizhifuabc/mybatis/x/dao/CustomBaseDao.java
@@ -0,0 +1,13 @@
+package io.github.lizhifuabc.mybatis.x.dao;
+
+import cn.xbatis.core.mvc.impl.DaoImpl;
+
+/**
+ * 基础 dao
+ *
+ * @author lizhifu
+ * @since 2025/4/16
+ */
+public class CustomBaseDao<T, ID> extends DaoImpl<T, ID> {
+
+}
---
+++ anan-core/anan-core-db/db-sdk-mybatis-x/src/main/java/io/github/lizhifuabc/mybatis/x/mapper/CustomBaseMapper.java
@@ -0,0 +1,12 @@
+package io.github.lizhifuabc.mybatis.x.mapper;
+
+import cn.xbatis.core.mybatis.mapper.MybatisMapper;
+
+/**
+ * 基础 mapper
+ *
+ * @author lizhifu
+ * @since 2025/4/16
+ */
+public interface CustomBaseMapper<T>  extends MybatisMapper<T>  {
+}
